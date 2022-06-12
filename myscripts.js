6e80dbe12f4c2e292e5f1eb2eaffb7204e2bea79
adebowale <ademibowaleadegboye@gmail.com>
2022-06-12 13:04:17 +0100

    javscritp modified

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,25 @@
-function myFunction() {
-          var x = document.getElementById("myLinks");
-          if (x.style.display === "block") {
-            x.style.display = "none";
-          } else {
-            x.style.display = "block";
-          }
-        }
+function openNav() {
+  document.getElementById("myLinks").style.width = "60%";
+  document.getElementById("myLinks").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("myLinks").style.display = "none";
+}
+
+
+
+
+
+
+
+
+// function myFunction() {
+//           var x = document.getElementById("myLinks");
+//           if (x.style.display === "block") {
+//             x.style.display = "none";
+//           } else {
+//             x.style.display = "block";
+//           }
+//         }
         
