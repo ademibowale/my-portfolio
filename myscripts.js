e7a7a9858aab7bf49570dd0aa6ee452732f58273
adebowale <ademibowaleadegboye@gmail.com>
2022-06-13 14:36:43 +0100

    add menu bar

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,10 @@
 function openNav() {
-  document.getElementById("myLinks").style.width = "60%";
+  document.getElementById("myLinks").style.width = "100%" ;
   document.getElementById("myLinks").style.display = "block";
-}
+  }
 
 function closeNav() {
-  document.getElementById("myLinks").style.display = "none";
+  document.getElementById("myLinks").style.width = "0%";
 }
 
 
@@ -13,13 +13,3 @@ function closeNav() {
 
 
 
-
-// function myFunction() {
-//           var x = document.getElementById("myLinks");
-//           if (x.style.display === "block") {
-//             x.style.display = "none";
-//           } else {
-//             x.style.display = "block";
-//           }
-//         }
-        
