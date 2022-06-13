2bb3b8be491dd71c1a8f7356c7ddfb5c72c295a2
adebowale <ademibowaleadegboye@gmail.com>
2022-06-13 22:49:15 +0100

    add solution to fix eslint

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 function openNav() {
-  document.getElementById("myLinks").style.width = "100%" ;
-  document.getElementById("myLinks").style.display = "block";
-  }
-
-function closeNav() {
-  document.getElementById("myLinks").style.width = "0%";
+  document.getElementById('myLinks').style.width = '100%';
+  document.getElementById('myLinks').style.display = 'block';
 }
-
-
-
-
-
-
-
+// eslint-disable-next-line no-unused-vars
+function closeNav() {
+  document.getElementById('myLinks').style.width = '0%';
+}
