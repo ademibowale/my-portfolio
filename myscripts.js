f11b570ae007b19ebbc8bc3f4850c57b14498e53
adebowale <ademibowaleadegboye@gmail.com>
2022-06-15 10:46:08 +0100

    modified html and implement javascript function

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,26 @@
-// eslint-disable-next-line no-unused-vars
-function openNav() {
+const menuBtn = document.querySelector('.openNav');
+const closeBtn = document.querySelector('.menu-list');
+const portfolioLink = document.querySelector('.portfolio-link');
+const aboutLink = document.querySelector('.about-link');
+const contactLink = document.querySelector('.contact-link');
+
+menuBtn.addEventListener('click', (e) => {
+  e.preventDefault();
   document.getElementById('myLinks').style.width = '100%';
   document.getElementById('myLinks').style.display = 'block';
-}
-// eslint-disable-next-line no-unused-vars
-function closeNav() {
+});
+
+closeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
   document.getElementById('myLinks').style.width = '0%';
-}
+});
+
+portfolioLink.addEventListener('click', () => {
+  document.getElementById('myLinks').style.width = '0%';
+});
+aboutLink.addEventListener('click', () => {
+  document.getElementById('myLinks').style.width = '0%';
+});
+contactLink.addEventListener('click', () => {
+  document.getElementById('myLinks').style.width = '0%';
+});
