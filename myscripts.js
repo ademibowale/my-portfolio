6e672d9a65c6b0b44280d372cd956c586e43f606
adebowale <ademibowaleadegboye@gmail.com>
2022-06-18 12:58:52 +0100

    implement javascript functionality

--- myscripts.js
+++ myscripts.js
@@ -30,30 +30,42 @@ const cardContainer = document.querySelector('.project')
 const data = [{id: '1',
                title: 'Profesional Art Printing Data',
                text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-               project_list: ['Java', 'bootstrap', 'ruby']},
+               project_list: ['Java', 'bootstrap', 'ruby'],
+               image: 'https://img.freepik.com/free-psd/logo-mockup-grey-wall_35913-2122.jpg?w=2000'},
                {id: '2',
                title: 'Profesional Art Printing Data',
                text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-               project_list: ['html', 'bootstrap', 'ruby']},
+               project_list: ['html', 'bootstrap', 'ruby'],
+               image: 'images/Snapshoot_Portfolio.png'},
                {id: '3',
                title: 'Profesional Art Printing Data',
                text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-               project_list: ['html', 'bootstrap', 'ruby']},
-               {id: '6',
+               project_list: ['html', 'bootstrap', 'ruby'],
+               image: 'images/Snapshoot_Portfolio.png'},
+               {id: '4',
                title: 'Profesional Art Printing Data',
                text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-               project_list: ['html', 'bootstrap', 'ruby']},
+               project_list: ['html', 'bootstrap', 'ruby'],
+               image: 'images/Snapshoot_Portfolio.png'},
                {id: '5',
                title: 'Profesional Art Printing Data',
                text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-               project_list: ['html', 'bootstrap', 'ruby']},
+               project_list: ['html', 'bootstrap', 'ruby'],
+               image: 'images/Snapshoot_Portfolio.png'},
                {id: '6',
                title: 'Profesional Art Printing Data',
                text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
                project_list: ['html', 'bootstrap', 'ruby'],
-               image: 'images/Snapshoot_Portfolio.png'}]
+               image: 'images/Snapshoot_Portfolio.png'},
+               {id: '7',
+               title: 'Multi-Post Stories',
+               text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+               project_list: ['css', 'html', 'bootstrap', 'ruby'],
+               image: 'images/Img_Placeholder22.png'},
+               ]
 
-data.forEach(item => {
+data.forEach((item, index) => {
+  if (index < 6) {
   cardContainer.innerHTML += `<li class="project-card bank">
   <div class="text-container ">
       <h3 class="project-title">${item.title}</h3>
@@ -66,6 +78,7 @@ data.forEach(item => {
   </div>
   <button id=${item.id} onclick='handlePopup(this.id)' class="button card-button" type="button">See Project</button>
 </li>`
+  }
 });
 
 const popup = document.querySelector('.display-popup')
@@ -77,31 +90,67 @@ const popup = document.querySelector('.display-popup')
     if (item.id == btnId) {
       popup.innerHTML=`<div class="display-popup-container">
    <div>
-   <nav>
+   <nav class='popup-nav'>
    <h2 class="post-title">${item.title}</h2>
    <a href="/" class="popup-close-btn">&times</a>
    </nav>
        <ul class="list">
-       <li>${item.project_list[0]}</li>
-       <li>${item.project_list[1]}</li>
-       <li>${item.project_list[2]}</li>
-   </ul>
+       </ul>
    </div>
-   <div>
-   <img src="${item.image}" alt="my logo"/>
+   <div class='img-container-fullscreen'>
+   <div class='popup-img-container'>
+   <img class='popup-img' src="${item.image}" alt="my logo"/>
    </div>
-   <p class="post-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent</p>
+   <div class='popup-content-fullscreen'>
+   <p class="post-text">${item.text}</p>
+   <div class='popup-btn-container-fullscreen'>
    <button type="button" class="project-button">See Live</button>
    <button type="button" class="project-button">See Source</button>
+   </div>
+   </div>
    </div>  
-</div> 
 </div>`
+const tech_list = popup.querySelector('.list')
+tech_list.innerHTML = ''
+console.log(tech_list)
+item.project_list.forEach(tech=>{
+  tech_list.innerHTML += `<li>${tech}</li>`
+ })
     }
    })
       
     }
 
-    const popClose = document.querySelector('.popup-close-btn')
-    popClose.addEventListener('click', () => {
-      popup.style.display = 'none'
+    // const popClose = document.querySelector('.popup-close-btn')
+    // popClose.addEventListener('click', () => {
+    //   popup.style.display = 'none'
+    // })
+
+
+
+
+    const extra = document.querySelector('.extra-class')
+    extra.addEventListener('click',(e) => {
+      handlePopup(e.target.id);
     })
+    const extra_fullscreen = document.querySelector('.extra-class-fullscreen')
+    extra_fullscreen.addEventListener('click',(e) => {
+      handlePopup(e.target.id);
+    })
+
+    const email = document.getElementById('my-form');
+const form = document.getElementById('my-form');
+const displayMsg = document.getElementById('form-message-error');
+displayMsg.innerHTML = 'Your email address should be in lowercase';
+form.addEventListener('submit', (event) => {
+  if (email.value !== email.value.toLowerCase()) {
+    event.preventDefault();
+    displayMsg.style.visibility = 'visible';
+    displayMsg.classList.add('error-msg');
+    setTimeout(() => {
+      displayMsg.style.visibility = 'hidden';
+    }, 3000);
+  } else {
+    displayMsg.style.visibility = 'hidden';
+  }
+});
